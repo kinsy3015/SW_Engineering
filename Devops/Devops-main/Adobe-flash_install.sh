664291f19098f788d66e695ed5efa2a08ffcfc46
shkim <62363841+kinsy3015@users.noreply.github.com>
2023-11-15 16:24:37 +0900

    Delete Devops/Devops-main directory

--- Devops/Devops-main/Adobe-flash_install.sh
+++
@@ -1,3 +0,0 @@
-sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
-sudo apt update -y
-sudo apt install adobe-flashplugin browser-plugin-freshplayer-pepperflash -y
--- Devops/Devops-main/sub_lime_txt_1804.sh
+++
@@ -1,6 +0,0 @@
-sudo apt update -y
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
-sudo apt update
-sudo apt install sublime-text
